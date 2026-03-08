1b5a5016e2a8e482f6463ddc9781f290a8380223
Mano-Prasad <manoprasadark@gmail.com>
2026-03-08 14:28:49 +0530

    First commit

---
+++ src/patternPrograms/QuestionNo_2.java
@@ -0,0 +1,25 @@
+package patternPrograms;
+
+
+               /*
+*****
+****
+***
+**
+*
+  
+            */
+
+public class QuestionNo_2 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 4; i >=0; i--) {
+			for(int j=0 ; j<=i; j++ ) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
